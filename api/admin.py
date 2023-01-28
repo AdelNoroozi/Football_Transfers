ab9667f24674a5c68dbba82e2c03d6018d2e462e
Adel <adel13no@gmail.com>
2023-01-28 17:15:43 +0330

    models added

---
+++ api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from api.models import Player, Team, Transfer, Popularities
+
+admin.site.register(Player)
+admin.site.register(Team)
+admin.site.register(Transfer)
+admin.site.register(Popularities)
